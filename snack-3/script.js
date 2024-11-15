b7cddc2c5967a31cd4c011ac3d8a29a6763a77bb
giuse <giuseppedefilippo03@gmail.com>
2024-11-15 13:40:33 +0100

    adjust

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,6 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 const arvuoto = [];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+/*let vuoto = numbers.map((numero) => {
+    return numero + 1;
+});
+console.log(vuoto)*/
 numbers.forEach((element) => {
     arvuoto.push(element + 1);
 })
